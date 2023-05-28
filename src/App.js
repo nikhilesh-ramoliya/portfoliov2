ee06a6844ccb0b98c3a054ce7154fcbf5903ff4b
nikhileshr-lanatus <nikhilesh.r@lanatussystems.com>
2023-05-28 11:48:19 +0530

    initial

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import Home from './Pages/Home/Home'
+import "./App.css"
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import About from './Pages/About/About';
+import Experience from './Pages/Experience/Experience';
+import Contact from './Pages/Contact/Contact';
+
+import { tsParticles } from "tsparticles-engine";
+import { Design } from './Design';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <div style={{ color: "white", position: "absolute", top: 0, left: 0, zIndex: -1000000 }}>
+        <Design />
+        hi
+      </div>
+      <div className="App">
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/home' element={<Home />} />
+          <Route path='/about' element={<About />} />
+          <Route path='/experience' element={<Experience />} />
+          <Route path='/contact' element={<Contact />} />
+
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ src/Design.jsx
@@ -0,0 +1,101 @@
+import { useCallback } from "react";
+import Particles from "react-tsparticles";
+import { loadFull } from "tsparticles";
+
+export const Design = () => {
+    const particlesInit = useCallback(async engine => {
+        console.log(engine);
+        await loadFull(engine);
+    }, []);
+
+    const particlesLoaded = useCallback(async container => {
+        await console.log(container);
+    }, []);
+
+    return (
+        <Particles
+            id="tsparticles"
+            init={particlesInit}
+            loaded={particlesLoaded}
+            options={{
+                background: {
+                    color: {
+                        value: "#000000",
+                    },
+                },
+                fpsLimit: 120,
+                interactivity: {
+                    events: {
+                        onClick: {
+                            enable: true,
+                            mode: "grab",
+                        },
+                        onHover: {
+                            enable: true,
+                            mode: "repulse",
+                        },
+                        resize: true,
+                    },
+                    modes: {
+                        push: {
+                            quantity: 4,
+                        },
+                        repulse: {
+                            distance: 150,
+                            duration: 0.4,
+                        },
+                        grab: {
+                            distance: 800,
+                            lineLinked: {
+                                opacity: 1,
+                            },
+                        },
+
+                    },
+                },
+                particles: {
+                    color: {
+                        value: "#00FFFF",
+                    },
+                    links: {
+                        color: "#ffffff",
+                        distance: 150,
+                        enable: true,
+                        opacity: 0.3,
+                        width: 1,
+                    },
+                    collisions: {
+                        enable: true,
+                    },
+                    move: {
+                        directions: "none",
+                        enable: true,
+                        outModes: {
+                            default: "bounce",
+                        },
+                        random: false,
+                        speed: 6,
+                        straight: false,
+                    },
+                    number: {
+                        density: {
+                            enable: true,
+                            area: 800,
+                        },
+                        value: 80,
+                    },
+                    opacity: {
+                        value: 0.5,
+                    },
+                    shape: {
+                        type: "circle",
+                    },
+                    size: {
+                        value: { min: 1, max: 5 },
+                    },
+                },
+                detectRetina: true,
+            }}
+        />
+    );
+};
